90c01a67971693ca01b45a4251c58e6f03e2edee
Gaming PC <sdf@gmail.com>
2023-06-11 13:11:49 +1000

    Add prac 1 files

--- prac_01/temperature.py
+++ prac_01/temperature.py
@@ -1,7 +1,3 @@
-"""
-CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
-"""
 
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
@@ -14,10 +10,8 @@ while choice != "Q":
         fahrenheit = celsius * 9.0 / 5 + 32
         print(f"Result: {fahrenheit:.2f} F")
     elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        pass
+        fahrenheit = float(input("Fahrenheit: "))
+        celsius = 5 / 9 * (fahrenheit - 32)
     else:
         print("Invalid option")
     print(MENU)
