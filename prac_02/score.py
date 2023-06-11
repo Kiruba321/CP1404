c6e8c0fc5985d9108cd284f8f516f2abb9ac105c
Gaming PC <sdf@gmail.com>
2023-06-11 21:45:48 +1000

    SRP: The function should not print it.

---
+++ prac_02/score.py
@@ -0,0 +1,15 @@
+def main():
+    score = float(input("Enter score: "))
+    print(Find_status(score))
+
+
+def Find_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
