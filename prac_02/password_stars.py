c4ad2497d95982aeab5526f914b31a5872c1c7bb
Gaming PC <sdf@gmail.com>
2023-06-11 20:45:55 +1000

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,9 @@
+length = 0
+
+while length < 10:
+    password = input("Enter your password: ")
+    length = len(password)
+    if length < 10:
+        print("Your password is weak!! Please choose a password with a minimum length of 10")
+    else:
+        print('*' * length)
