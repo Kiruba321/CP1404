94ee6b2f56f9a45e72ecd57f5be0319f9b22a210
Gaming PC <sdf@gmail.com>
2023-06-11 20:51:13 +1000

    Inside main fucntion

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,11 @@
-length = 0
+def main():
+    length = 0
 
-while length < 10:
-    password = input("Enter your password: ")
-    length = len(password)
-    if length < 10:
-        print("Your password is weak!! Please choose a password with a minimum length of 10")
-    else:
-        print('*' * length)
+    while length < 10:
+        password = input("Enter your password: ")
+        length = len(password)
+        if length < 10:
+            print("Your password is weak!! Please choose a password with a minimum length of 10")
+        else:
+            print('*' * length)
+main()
