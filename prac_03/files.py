152b1ca1716f8c9b18211d6bb752a024664ebebf
Gaming PC <sdf@gmail.com>
2023-06-18 19:06:25 +1000

    files

---
+++ prac_03/files.py
@@ -0,0 +1,31 @@
+# Q1
+output_file = 'name.TXT'
+out_file = open(output_file, 'w')  # open file for writing
+name = input('Enter your name: ')
+print('you name is: ', name, file=out_file)
+out_file.close()
+
+
+# Q2
+open_file = open("name.txt", "r")
+name = open_file.read().strip()
+print("Your name is", name)
+open_file.close()
+
+
+# Q3
+open_file = open("numbers.TXT", "r")
+first_line = int(open_file.readline())
+second_line = int(open_file.readline())
+result = first_line + second_line
+print("Sum of line 1 & 2 is ",result)
+
+
+# Q4
+open_file = open("numbers.TXT", "r")
+total = 0
+for line in open_file:
+    i = int(line)
+    total += i
+print("The sum of all lines are",total)
+open_file.close()
