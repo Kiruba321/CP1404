d048a61f9334701bedb87c0b436747b3c0d09130
Gaming PC <sdf@gmail.com>
2023-06-25 17:53:43 +1000

    Converting Data strings to Lists, append update to the subjects list.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,11 +8,12 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -21,9 +22,14 @@ def get_data():
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject.append(parts)  # Append the parsed parts to the subjects list
     input_file.close()
+    return subject
+
+
+def display_data(subjects):
+    for subject in subjects:
+        print("{} is taught by {:12} and has {:3} students".format(*subject))
 
 
 main()
