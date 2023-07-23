3ad921d81a345282118e66d0a655a639f6541a3d
Gaming PC <sdf@gmail.com>
2023-07-23 20:44:39 +1000

    Complete first part of More Guitars exercise

---
+++ prac_07/myguitars.py
@@ -0,0 +1,44 @@
+class Guitar:
+
+    def __init__(self, name, year, cost):
+        """Construct a Guitar from the given values."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of a Guitar."""
+        return f"{self.name}, {self.year}, ${self.cost:.2f}"
+
+    def __lt__(self, other):
+        return self.year < other.year
+
+
+def read_guitars_from_file(filename):
+    guitars = []
+    with open(filename, 'r') as file:
+        for line in file:
+            name, year, cost = line.strip().split(',')
+            year = int(year)
+            cost = float(cost)
+            guitars.append(Guitar(name, year, cost))
+    return guitars
+
+
+def main():
+    filename = 'guitars.csv'
+    guitars = read_guitars_from_file(filename)
+
+    print("All guitars:")
+    for guitar in guitars:
+        print(guitar)
+
+    guitars.sort()
+
+    print("\nGuitars sorted by year (oldest to newest):")
+    for guitar in guitars:
+        print(guitar)
+
+
+if __name__ == "__main__":
+    main()
