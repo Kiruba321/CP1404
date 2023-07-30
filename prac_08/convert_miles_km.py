6a02d22edd00525c4b68fae1f85b11b32c8649eb
Gaming PC <sdf@gmail.com>
2023-07-31 01:14:46 +1000

    Implement Miles to Kilometers Converter App

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -13,13 +13,22 @@ class MilesConverterApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_convert(self):
+    def handle_convert(self, value):
         try:
-            miles = float(self.root.ids.input_miles.text)
+            miles = float(value) if value else 0.0
             kilometers = miles * MILES_TO_KM
             self.output_text = f"{kilometers:.2f} km"
         except ValueError:
-            self.output_text = "Invalid input"
+            self.output_text = "0.0 km"
+
+    def handle_increment(self, increment):
+        try:
+            miles = float(self.root.ids.input_miles.text) if self.root.ids.input_miles.text else 0.0
+            miles += increment
+            self.root.ids.input_miles.text = str(miles)
+            self.handle_convert(self.root.ids.input_miles.text)
+        except ValueError:
+            self.output_text = "0.0 km"
 
 
 if __name__ == "__main__":
