447ad11993bea7ab2ca05a2ae6ac431c38d8f1ba
Gaming PC <sdf@gmail.com>
2023-07-30 21:36:13 +1000

    Add button handler for 'Greet' button in box_layout_demo.py

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        user_name = self.root.ids.user_name_input.text.strip()
+        if user_name != "":
+            self.root.ids.greeting_label.text = f"Hello, {user_name}!"
+        else:
+            self.root.ids.greeting_label.text = "Please enter your name."
+
+
+if __name__ == "__main__":
+    BoxLayoutDemo().run()
